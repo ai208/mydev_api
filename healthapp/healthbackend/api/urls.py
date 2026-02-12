53dd0e9865502b50b9981777d3e8956ab34b295f
ai208 <yasu6328hand@gmail.com>
2026-02-12 22:06:51 +0900

    ch7,8,9 fin

--- healthapp/healthbackend/api/urls.py
+++ healthapp/healthbackend/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from. import views
 
 urlpatterns = [
-    path('healths/',views.HealthList.as_view()),
+    path('healths/',views.HealthListCreate.as_view()),
+    path('healths/<int:pk>',views.HealthRetrieveUpdateDestroy.as_view()),
 ]
--- healthapp/healthbackend/api/views.py
+++ healthapp/healthbackend/api/views.py
@@ -1,9 +1,20 @@
-from rest_framework import generics
+from rest_framework import generics,permissions
 from.serializers import HealthSerializer
 from health.models import Health
 # Create your views here.
-class HealthList(generics.ListAPIView):
+class HealthListCreate(generics.ListCreateAPIView):
     serializer_class = HealthSerializer
+    permission_classes = [permissions.IsAuthenticated]
     def get_queryset(self):
         user = self.request.user
         return Health.objects.filter(user=user).order_by('created')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class HealthRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = HealthSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    def get_queryset(self):
+        user = self.request.user
+        return Health.objects.filter(user=user)
